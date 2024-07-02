221d593916cf5a42962259bab4625c10019509f6
Tomilin Ivan <i.tomilin.91@yandex.ru>
2024-07-02 20:32:49 +0300

    create DZ 23.2

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from store.apps import StoreConfig
-from store.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from store.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, \
+    CategoryListView
 
 app_name = StoreConfig.name
 
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='create'),
     path('', ProductListView.as_view(), name='list'),
-    path('view/<int:pk>/', ProductDetailView.as_view(), name='view'),
+    path('view/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='view'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
+    path('category/', CategoryListView.as_view(), name='category'),
 ]
